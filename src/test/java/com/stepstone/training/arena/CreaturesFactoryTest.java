8572c118a47515055761bb1737d808b8fa51efd7
daniel.tendaj <daniel.tendaj@totaljobsgroup.com>
2018-08-02 08:08:12 +0200

    New test for CreaturesFactory.

--- src/test/java/com/stepstone/training/arena/CreaturesFactoryTest.java
+++ src/test/java/com/stepstone/training/arena/CreaturesFactoryTest.java
@@ -5,8 +5,24 @@ import static org.junit.Assert.assertTrue;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-class CreaturesFactoryTest {
+import java.util.HashMap;
+import java.util.Map;
 
+public class CreaturesFactoryTest {
 
+    @Test
+    public void shouldFactoryReturnElf() {
+        //given
+        CreaturesFactory creaturesFactory = new CreaturesFactory();
+
+        Map<ProtectionItem, Integer> map = new HashMap<>();
+        map.put(ProtectionItem.HELMET, 1);
+
+        //when
+        Creature elf = creaturesFactory.generate(CreatureType.ELF,10, map);
+
+        //then
+        assertTrue(elf.getType() == CreatureType.ELF);
+    }
 
 }
